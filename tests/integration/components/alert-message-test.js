7cb474873763081fb49141f60d167beb7d026240
sean <seangores@gmail.com>
2016-07-21 17:27:33 -0600

    Add alert-message integration tests

--- tests/integration/components/alert-message-test.js
+++ tests/integration/components/alert-message-test.js
@@ -1,11 +1,13 @@
 import {moduleForComponent, test} from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
 
+const DISMISS_SELECTOR = ".dialog-dismiss";
+
 moduleForComponent("alert-message", "Integration | Component | alert message", {
 	integration: true
 });
 
-test("it renders", function (assert) {
+test("it renders", function(assert) {
 	// Set any properties with this.set("myProperty", "value");
 	// Handle any actions with this.on("myAction", function(val) { ... });
 
@@ -21,4 +23,29 @@ test("it renders", function (assert) {
 	`);
 
 	assert.equal(this.$().text().trim(), "template block text");
+});
+
+test("it is a `<dialog>` element", function(assert) {
+	this.render(hbs`{{alert-message}}`);
+
+	assert.equal(this.tagName.toLowerCase(), "dialog");
+});
+
+test("it has an `open` attribute by default", function(assert) {
+	this.render(hbs`{{alert-message}}`);
+
+	assert.ok(this.$().attr("open"));
+});
+
+test("it has a dismiss button when the user allows dismissal", function(assert) {
+	this.render(hbs`{{alert-message}}`);
+
+	assert.ok(this.$(DISMISS_SELECTOR).length > 1);
+});
+
+test("it dismisses when the user calls dismiss", function(assert) {
+	this.render(hbs`{{alert-message}}`);
+
+	this.$(DISMISS_SELECTOR).click();
+	assert.equal(this.$().is(":visible"), false);
 });
