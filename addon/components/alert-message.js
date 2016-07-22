65dd18426778589026d654b803c3c61ee4bc4242
sean <seangores@gmail.com>
2016-07-21 18:32:05 -0600

    alert-message component: add base files

---
+++ addon/components/alert-message.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+import layout from "../templates/components/alert-message";
+
+export default Ember.Component.extend({
+	tagName: "dialog",
+	layout: layout
+});
---
+++ app/components/alert-message.js
@@ -0,0 +1,1 @@
+export {default} from "ember-dialogue/components/alert-message";
